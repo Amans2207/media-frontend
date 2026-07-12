c99d56c7d6df6286bd001654bd8e64564ee3cefa
Aman <Aman@example.com>
2026-07-12 15:00:06 +0530

    fix(auth): explicitly configure supabase to persist session in localStorage

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,4 +3,11 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://ktvgvdhilqmyobxkyipg.supabase.co'
 const supabaseAnonKey = 'sb_publishable_Tyj_hJ_DZGiabdBGtohfig_2iQnhIjq'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: window.localStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true
+  }
+})
